8f7018c2aeee4469f5634b95308d5d754bb247e0
rushilsheth <rsheth@berkeley.edu>
2016-10-19 19:09:17 -0700

    created train and test using sample, saved them to an Rdata file - R

---
+++ code/scripts/train_test-script.R
@@ -0,0 +1,20 @@
+#this code creates a random ordered vector consisting of 300 trues and 100 falses
+
+set.seed (1)
+
+#train set will be size 300
+true <- !logical(300)
+false <- logical(100)
+vec <- c(true, false)
+
+
+train <- sample(vec, replace = FALSE)
+#test set is the rest of 100 observations
+test <- (! train )
+
+#store train and test data in an Rdata file
+#this data will be used everytime we build a model
+
+save(train, test file = "data/train_test.RData")
+
+
