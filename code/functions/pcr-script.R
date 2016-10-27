12949b4d4b4eeac9d8530015576042f500e2d26c
rushilsheth <rsheth@berkeley.edu>
2016-10-26 17:53:48 -0700

    need to make it possible for all predictors

--- code/functions/pcr-script.R
+++ code/functions/pcr-script.R
@@ -1,22 +1,52 @@
-#this will be the file for pcr-script
+# This script will run a pcr regression
 
-library(pls)
+# Load necessary libraries and variables
+library (pls)
+# Load my train and testing data set
+load("data/train_test.RData")
+# Load scaled-credit data
+scaled_credit <- read.csv("data/scaled-credit.csv")
+scaled_credit$X <- NULL
 
-set.seed(2)
+set.seed (72838)
 
-#read in scaled credit data
-credit <- read.csv("data/scaled-credit.csv")
+# Now begin pcr regression method
 
-pcr.fit = pcr(Balance∼., data=credit ,scale=TRUE , validation ="CV")
+# Do I need scale = TRUE in pcr call????
 
-pcr_best_lambda <- min(pcr.fit$validation$PRESS)
+# Running Cross Validation with pcr
+cv_pcr <- pcr(Balance ~ ., data = train_set, validation = 'CV')
+	 
+# Find min lambda
+lambda_min_pc <- min(cv_pc$validation$PRESS)
 
-save(pcr_best_lamda, pcr.fit, file = "data/pcr.RData")
-
-
-#plot pcr output
-png("images/pcr_plots.png")
-validationplot(pcr.fit, val.type = "MSEP")
+# Plot pcr regression
+png('images/pcr_plot.png')
+validationplot(cv_pc, val.type = 'MSEP')
 dev.off()
 
-#need to do step 5 and 6
+# Calculate MSE
+pc_predict <- predict(cv_pc, ncomp = lambda_min_pc, newdata = test_set)
+pc_MSE <- mean((pc_predict - test_set$Balance)^2)
+
+# Re-Fit the Model on the Full Data Set and use best lambda from above
+pc_fit <- pcr(Balance ~ ., data = scaled_credit, ncomp = lambda_min_pc )
+pc_coef_full <- coef(pc_fit)
+	 
+# Save Data and Generate an informative Output in txt file
+save(lambda_min_pc, cv_pc, pc_MSE, pc_coef_full,file = 'data/RData-files/pc-regression.RData')
+	 
+sink('data/outputs/pc-regression-output.txt')
+cat('Output of PCR with 10-fold CV on the Full Data Set\n')
+print(summary(cv_pc))
+cat('\nMinimum Lambda for PCR\n')
+print(lambda_min_pc)
+cat('\nPCR MSE of Test Data Set\n')
+print(pc_MSE)
+cat('\nCoefficients of using PCR Model on Full Data Set\n')
+print(pc_coef_full)
+sink()
+
+
+
+
