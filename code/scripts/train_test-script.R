b28de1db923afd1386eca65084c5f9447bd9239c
rushilsheth <rsheth@berkeley.edu>
2016-10-26 18:08:06 -0700

    changed trainin to train

--- code/scripts/train_test-script.R
+++ code/scripts/train_test-script.R
@@ -16,12 +16,12 @@ train <- sample(vec, replace = FALSE)
 test <- (! train )
 
 #create the training and test set from the data itself
-training_set <- credit[train,]
+train_set <- credit[train,]
 
 test_set <- credit[test,]
 
 #store train and test data in an Rdata file
 #this data will be used everytime we build a model
-save(training_set, test_set, file = "data/train_test.RData")
+save(train_set, test_set, file = "data/train_test.RData")
 
 
