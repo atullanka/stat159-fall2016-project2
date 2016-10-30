59d9e85cded31eb87fc7c99b2f16e81a035be2e9
atullanka <atul.lanka@berkeley.edu>
2016-10-30 01:13:16 -0700

    Update plsr_regression.R

--- code/scripts/plsr_regression.R
+++ code/scripts/plsr_regression.R
@@ -11,7 +11,7 @@ test_y = test_data$Balance
 pls_fit = plsr(Balance ~ ., data=training_data,scale=TRUE, validation="CV")
 
 # Find the number of components that yields the lowest MSEP
-comp = which.min(pls_fit$validation$PRESS)
+min.comp = which.min(pls_fit$validation$PRESS)
 
 # Save validation plot to png file
 png(filename="images/plsr_validationplot.png")
@@ -19,15 +19,15 @@ validationplot(pls_fit, val.type="MSEP")
 dev.off()
 
 # Find the test mse
-pls_pred = predict(pls_fit, test_x, ncomp=comp)
+pls_pred = predict(pls_fit, test_x, ncomp=min.comp)
 mse = mean((pls_pred-test_y)^2)
 
 # Find the coefficients for the best value of the number of components
-pls_out = plsr(Balance ~ ., data=scaled_data ,scale=TRUE,ncomp=comp)
-coeff = pls_out$coefficients[, , comp]
+pls_out = plsr(Balance ~ ., data=scaled_data ,scale=TRUE,ncomp=min.comp)
+coeff = pls_out$coefficients[, , min.comp]
 
 # Save output objects to RData file
-save(pls_fit, comp, mse, coeff, file="data/plsr.RData")
+save(pls_fit, min.comp, mse, coeff, file="data/plsr.RData")
 
 # Write coefficients, best number of components, and mse to a text file
 sink("data/plsr.txt")
@@ -35,5 +35,5 @@ pander(coeff)
 writeLines("\nTest MSE:\n")
 mse
 writeLines("\nBest Number of Components:\n")
-comp
+min.comp
 sink()
