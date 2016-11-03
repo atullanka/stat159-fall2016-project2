0dbebfed2dede37b01bc62d3214f144bf72584f8
Atul Lanka <atul.lanka@berkeley.edu>
2016-11-03 14:08:54 -0700

    changes made to files and analysis section added

--- code/scripts/ols_regression.R
+++ code/scripts/ols_regression.R
@@ -3,9 +3,12 @@ scaled_data <- read.csv("../../data/scaled-credit.csv")[, -1]
 # Fit OLS model:
 ols_model <- lm(Balance ~ .,data = scaled_data)
 OLS_summary <-summary(ols_model)
+ols_mse = mean(OLS_summary$residuals^2)
 sink('../../data/ols_regression_output.txt')
 cat('OLS model summary \n')
 OLS_summary
+cat('MSE \n')
+ols_mse
 sink()
 
-save(OLS_summary, file='../../data/OLS_regression.RData')
+save(OLS_summary,ols_mse, file='../../data/OLS_regression.RData')
